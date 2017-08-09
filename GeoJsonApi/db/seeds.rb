72fde1a759182f8d3ae4848372bff0bc5e30448a
Daniel <daniel.j.fletcher2@gmail.com>
2017-08-09 15:31:19 -0700

    Ran migration, wrote seed file

--- GeoJsonApi/db/seeds.rb
+++ GeoJsonApi/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+file = File.read('../../intersections_array_points.txt')
+points = eval(file)
+
+points.each_with_index do |point, idx|
+  Intersection.create!(
+    longitude: point[0],
+    latitude: point[1]
+  )
+  puts "completed point #{idx + 1} of #{points.length}" if idx % 10000 == 0
+end
