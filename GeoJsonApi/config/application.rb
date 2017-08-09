997af44ce3932b80c5752311c1b1f388c4676bb0
Daniel <daniel.j.fletcher2@gmail.com>
2017-08-09 16:25:50 -0700

    Add rack-cors ruby gem to allow Rails app to accept CORS get requests

--- GeoJsonApi/config/application.rb
+++ GeoJsonApi/config/application.rb
@@ -17,6 +17,7 @@ Bundler.require(*Rails.groups)
 
 module GeoJsonApi
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -31,5 +32,14 @@ module GeoJsonApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # rack-cors gem used to allow CORS below: (https://github.com/cyu/rack-cors)
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get]
+      end
+    end
+
   end
 end
