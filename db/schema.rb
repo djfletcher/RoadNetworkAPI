95105303af0fd5e85d27e141d952bb31c97ce800
Daniel <daniel.j.fletcher2@gmail.com>
2017-08-09 19:10:02 -0700

    Begin writing road edges seeding logic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809222223) do
+ActiveRecord::Schema.define(version: 20170810013846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,5 +25,17 @@ ActiveRecord::Schema.define(version: 20170809222223) do
 
   add_index "intersections", ["latitude"], name: "index_intersections_on_latitude", using: :btree
   add_index "intersections", ["longitude"], name: "index_intersections_on_longitude", using: :btree
+
+  create_table "road_edges", force: :cascade do |t|
+    t.integer  "intersection1_id", null: false
+    t.integer  "intersection2_id", null: false
+    t.string   "street_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "road_edges", ["intersection1_id"], name: "index_road_edges_on_intersection1_id", using: :btree
+  add_index "road_edges", ["intersection2_id"], name: "index_road_edges_on_intersection2_id", using: :btree
+  add_index "road_edges", ["street_name"], name: "index_road_edges_on_street_name", using: :btree
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'json'
+
 
 NORTHEAST = [-122.3462101020676, 37.8123578039731]
 SOUTHWEST = [-122.62458645470844, 37.698000517492105]
@@ -14,6 +16,13 @@ def within_sf(latitude, longitude)
   SOUTHWEST[0] < longitude && longitude < NORTHEAST[0]
 end
 
+def is_intersection?(latitude, longitude)
+  Intersection.exists?(latitude: latitude, longitude: longitude)
+end
+
+
+# INTERSECTIONS
+# =================
 file = File.read('../intersections_and_endpoints_array.txt')
 points = eval(file)
 
@@ -28,3 +37,26 @@ points.each_with_index do |point, idx|
   end
   puts "completed point #{idx + 1} of #{points.length}" if idx % 10000 == 0
 end
+
+
+
+# ROAD EDGES
+# ================
+# file = File.read('../san-francisco_california.imposm-geojson/san-francisco_california_roads.geojson')
+# roads = JSON.parse(file)['features']
+#
+# roads.each_with_index do |road, idx|
+#   intersection1, intersection2 = nil, nil
+#   road['geometry']['coordinates'].each do |coord|
+#     longitude = coord[0]
+#     latitude = coord[1]
+#     if within_sf(latitude, longitude) && is_intersection?(latitude, longitude)
+#
+#     end
+#   end
+#
+#   # Add 1 to start point and end point of each road, so that they will be counted as intersections
+#   road_start = road['geometry']['coordinates'].first
+#   road_end = road['geometry']['coordinates'].last
+#   [road_start, road_end].each { |coord| points[coord.to_s] += 1 }
+# end
