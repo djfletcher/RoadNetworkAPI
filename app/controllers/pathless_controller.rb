22f21e7b941597c6c60e63457b62bc3e12c38ae8
Daniel <daniel.j.fletcher2@gmail.com>
2017-08-13 15:02:30 -0700

    Add gem http

--- app/controllers/pathless_controller.rb
+++ app/controllers/pathless_controller.rb
@@ -1,7 +1,10 @@
 class PathlessController < ApplicationController
 
   def index
-    msg = { status: 303, message: "Please try any of the following urls: 'http://road-network-api.herokuapp.com/intersections', 'http://road-network-api.herokuapp.com/road_edges'" }
+    msg = {
+      status: 303,
+      message: "Please try any of the following urls: 'http://road-network-api.herokuapp.com/intersections', 'http://road-network-api.herokuapp.com/road_edges'" 
+    }
     render json: msg
   end
 
